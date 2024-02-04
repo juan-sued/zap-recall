c67e1a0042d95bb5b507cba2af21a8f64796dd3b
Juan Sued <juansued19@gmail.com>
2024-02-04 13:43:53 -0300

    feature/ajustes e melhorias

--- src/Mock/ZapMocks.ts
+++ src/Mock/ZapMocks.ts
@@ -7,7 +7,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Este questionário explora os diferentes estágios do ciclo celular e o processo de divisão celular.',
     difficulty: Difficulties.EASY,
-    percentEndings: 90,
+    endings: 90,
     attempts: 25,
     creatAt: '10/10/2023',
     updatedAt: '12/10/2023',
@@ -19,7 +19,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Analise os princípios básicos da genética descobertos por Gregor Mendel e como eles se aplicam a diferentes organismos.',
     difficulty: Difficulties.MEDIUM,
-    percentEndings: 60,
+    endings: 60,
     attempts: 50,
     creatAt: '15/09/2023',
     updatedAt: '18/09/2023',
@@ -31,7 +31,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Explore o funcionamento do sistema circulatório humano, incluindo o coração, vasos sanguíneos e circulação sanguínea.',
     difficulty: Difficulties.HARD,
-    percentEndings: 30,
+    endings: 30,
     attempts: 100,
     creatAt: '20/08/2023',
     updatedAt: '22/08/2023',
@@ -43,7 +43,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Estude os diferentes tipos de ecossistemas aquáticos, incluindo oceanos, rios, lagos e pântanos.',
     difficulty: Difficulties.MEDIUM,
-    percentEndings: 70,
+    endings: 70,
     attempts: 40,
     creatAt: '05/11/2023',
     updatedAt: '07/11/2023',
@@ -55,7 +55,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Aprofunde-se no processo de metabolismo celular, incluindo catabolismo e anabolismo de diferentes nutrientes.',
     difficulty: Difficulties.HARD,
-    percentEndings: 20,
+    endings: 20,
     attempts: 80,
     creatAt: '08/11/2023',
     updatedAt: '10/11/2023',
@@ -67,7 +67,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Estude os aspectos moleculares da genética, incluindo estrutura do DNA, replicação e expressão gênica.',
     difficulty: Difficulties.MEDIUM,
-    percentEndings: 50,
+    endings: 50,
     attempts: 60,
     creatAt: '12/11/2023',
     updatedAt: '15/11/2023',
@@ -79,7 +79,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Conheça a anatomia e fisiologia do sistema nervoso, incluindo o cérebro, medula espinhal e nervos periféricos.',
     difficulty: Difficulties.HARD,
-    percentEndings: 30,
+    endings: 30,
     attempts: 100,
     creatAt: '18/11/2023',
     updatedAt: '20/11/2023',
@@ -91,7 +91,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Explore os mecanismos e evidências da evolução biológica e a diversidade de vida na Terra.',
     difficulty: Difficulties.EASY,
-    percentEndings: 90,
+    endings: 90,
     attempts: 20,
     creatAt: '22/11/2023',
     updatedAt: '25/11/2023',
@@ -103,7 +103,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Estude os microrganismos, incluindo bactérias, vírus e fungos, e seu papel em diferentes ambientes e processos biológicos.',
     difficulty: Difficulties.MEDIUM,
-    percentEndings: 70,
+    endings: 70,
     attempts: 35,
     creatAt: '30/11/2023',
     updatedAt: '02/12/2023',
@@ -115,7 +115,7 @@ const ZAP_BASIC: IZapBasic[] = [
     description:
       'Aprofunde-se no sistema imunológico, incluindo células imunes, resposta imune e imunização.',
     difficulty: Difficulties.HARD,
-    percentEndings: 40,
+    endings: 40,
     attempts: 90,
     creatAt: '05/12/2023',
     updatedAt: '07/12/2023',
